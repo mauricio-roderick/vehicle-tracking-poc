20a9f2a4cd5c3baf8733c9994290a4f689fe4270
erick-mauricio <mauricio.roderick@yahoo.com>
2016-07-20 19:00:00 +0800

    mock data config

--- config/config.js
+++ config/config.js
@@ -11,9 +11,63 @@ module.exports = {
 		tcp_port: 8056,
 		topic: 'reekoh/data'
 	},
+	mock_data: {
+		deviceCount: 50,
+		countries: {
+			'PH' : {
+				border: {
+					lat: {
+						max: 14.5570,
+						min: 14.5145
+					},
+					lng: {
+						max: 121.0614,
+						min: 121.0019
+					}
+				}
+			},
+			'US' : {
+				border: {
+					lat: {
+						max: 44.3081,
+						min: 33.1743
+					},
+					lng: {
+						max: -91.9336,
+						min: -113.0713	
+					}
+				}
+			},
+			'IN' : {
+				border: {
+					lat: {
+						max: 21.7391,
+						min: 	19.1348
+					},
+					lng: {
+						max: 82.5073,
+						min: 78.4644
+					}
+				}
+			},
+			'MX' : {
+				border: {
+					lat: {
+						max: 24.2970,
+						min: 21.4224
+					},
+					lng: {
+						max: -100.0854,
+						min: -103.9307
+					}
+				}
+			}
+		}
+	}
 	mock_interval: 10000,
     mongo: {
         local_url: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/local',
-        url: process.env.MONGO_URL || 'mongodb://reekohdev:Reekoh2016@ds015398.mlab.com:15398/reekoh-mongo-test'
+        test_url: process.env.MONGO_URL || 'mongodb://reekohdev:Reekoh2016@ds015398.mlab.com:15398/reekoh-mongo-test',
+        url: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/local',
     }
 };
