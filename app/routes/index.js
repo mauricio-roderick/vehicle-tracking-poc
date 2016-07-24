93a2b5f865744a23cf19623ffd5e36550b35e9cc
erick-mauricio <mauricio.roderick@yahoo.com>
2016-07-24 21:55:08 +0800

    updated mock data script - updates for meitrack device

--- app/routes/index.js
+++ app/routes/index.js
@@ -75,13 +75,13 @@ router.get('/index', function(req, res, next) {
 							});
 
 						marker.device = marker.device_info._id;
-						marker.coordinates.lat += chance.bool() ? -(increment) : (increment);
-						marker.coordinates.lon += chance.bool() ? -(increment) : (increment);
-						marker.timestamp = new Date().toJSON();
-						marker.speed = chance.integer({min: 20, max: 30});
+						marker.device_data.coordinates.lat += chance.bool() ? -(increment) : (increment);
+						marker.device_data.coordinates.lon += chance.bool() ? -(increment) : (increment);
+						marker.device_data.speed = chance.integer({min: 20, max: 30});
+						marker.timestamp = moment().toISOString();
 
-						console.log(marker.device);
-						console.log(marker.coordinates);
+						delete marker._id;
+						
 						request.post({
 							url: url,
 							json: marker
@@ -100,7 +100,7 @@ router.get('/index', function(req, res, next) {
 		if(result.mockDevices.length) {
 			console.log(`Found ${result.mockDevices.length} mock devices.`);
 
-			// setTimeout(moveMockDevices, config.mock.movement_interval);			
+			setTimeout(moveMockDevices, config.mock.movement_interval);			
 		} else {
 			console.log('No mock devices found');
 		}
